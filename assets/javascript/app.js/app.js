32985db522b16f424096cbe33f387c016c8002d6
aguzt <aggz97@icloud.com>
2019-03-24 22:59:00 -0400

    process

---
+++ assets/javascript/app.js/app.js
@@ -0,0 +1,26 @@
+var statements = [
+    {
+        prompt: "Mexico achieved independence before the US./n (a) true/n\(b) false" 
+        answer:
+    }
+    {
+        prompt: "Alexander The Great became pharaoh of Egypt./n (a) true/n\(b) false" 
+        answer:
+    }
+    {
+        prompt: "The parthenon was built before the Colossus of Rhodes./n (a) true/n\(b) false" 
+        answer:
+    }
+    {
+        prompt: "No US presidents have been born in Missouri./n (a) true/n\(b) false" 
+        answer:
+    }
+    {
+        prompt: "William Wallace lived before king Macbeth/n (a) true/n\(b) false" 
+        answer:
+    }
+   
+] 
+var score = 0; 
+for(var i=0; i < questions.length; i++) 
+var response = window.prompt
